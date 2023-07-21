fba0aa42eb952d7760bffc01a89a123d95191baa
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-07-20 22:30:37 -0600

    draw polys

--- Rasterizer.py
+++ Rasterizer.py
@@ -1,21 +1,58 @@
-from gl import Renderer,V3,V3,color
+from gl import Renderer,V2,V3,color
 from obj import Obj
 import shaders
 import random
 
 
-width = 1920
-height = 1080
+def drawPoly(poligon):
+    i = 0
+    while (i+1<len(poligon)):
+        rend.glLine(poligon[i],poligon[i+1])
+        if i == len(poligon) - 2:
+            rend.glLine(poligon[i+1],poligon[0])
+        i+= 1 
+
+width = 920
+height = 480
 
 rend = Renderer(width,height)
 
-rend.vertexShader = shaders.vertexShader
-rend.fragmentShader = shaders.fragmentShader
+Poligon1 = [V2(165, 380), V2(185, 360),
+            V2(180, 330), V2(207, 345), 
+            V2(233, 330), V2(230, 360), 
+            V2(250, 380), V2(220, 385), 
+            V2(205, 410), V2(193, 383)]
 
-rend.glLoadModel("P1.obj", 
-                 translate=(width/2,height/4,0), 
-                 scale=(500,500,500))
+Poligon2 = [V2(321, 335), V2(288, 286), 
+            V2(339, 251), V2(374, 302)]
 
-rend.glRender()
+Poligon3 = [V2(377, 249), V2(411, 197), V2(436, 249)]
+
+Poligon4 = [V2(413, 177), V2(448, 159), 
+            V2(502, 88), V2(553, 53), 
+            V2(535, 36), V2(676, 37), 
+            V2(660, 52), V2(750, 145), 
+            V2(761, 179), V2(672, 192), 
+            V2(659, 214), V2(615, 214), 
+            V2(632, 230), V2(580, 230), 
+            V2(597, 215), V2(552, 214), 
+            V2(517, 144), V2(466, 180)]
+
+Poligon5 = [V2(682, 175), V2(708, 120), 
+            V2(735, 148), V2(739, 170)]
+
+# i = 0
+# while (i+1<len(Poligon1)):
+#     rend.glLine(Poligon1[i],Poligon1[i+1])
+#     if i == len(Poligon1) - 2:
+#         rend.glLine(Poligon1[i+1],Poligon1[0])
+#     i+= 1 
+
+drawPoly(Poligon1)
+drawPoly(Poligon2)
+drawPoly(Poligon3)
+drawPoly(Poligon4)
+drawPoly(Poligon5)
+
 
 rend.glFinish("output.bmp")
--- gl.py
+++ gl.py
@@ -52,30 +52,13 @@ class Renderer(object):
 
         self.glColor(1,1,1)
 
-        self.vertexShader = None
-        self.fragmentShader = None
-
-        self.primitiveType = TRIANGLES
         self.vertexBuffer = []
 
-        self.objects = []
 
     def glAddVertices(self, vertices):
         for vert in vertices:
             self.vertexBuffer.append(vert)
 
-    def glPrimitiveAssembly(self, tverts):
-        primitives = []
-        
-        if self.primitiveType == TRIANGLES:
-            for i in range(0,len(tverts),3):
-                triangle = []
-                triangle.append(tverts[i])
-                triangle.append(tverts[i+1])
-                triangle.append(tverts[i+2])
-                primitives.append(triangle)
-        
-        return primitives
 
     def glClearColor(self, r, g, b):
         self.clearColor = color(r,g,b)
@@ -96,19 +79,6 @@ class Renderer(object):
         self.glLine(v0,v1,clr or self.currColor)
         self.glLine(v1,v2,clr or self.currColor)
         self.glLine(v2,v0,clr or self.currColor)
-
-    def glModelMatrix(self, translate = (0,0,0), scale = (1,1,1)):
-        translation = [[1,0,0,translate[0]],
-                     [0,1,0,translate[1]],
-                     [0,0,1,translate[2]],
-                     [0,0,0,1]]
-        
-        scaleMat = [[scale[0],0,0,0],
-                    [0,scale[1],0,0],
-                    [0,0,scale[2],0],
-                    [0,0,0,1]]
-        
-        return metha.multiplymatrix(translation,scaleMat)
 
     def glLine(self,v0,v1,clr=None):
         #Bresenham line algorith
@@ -173,63 +143,6 @@ class Renderer(object):
                     y -= 1
                     
                 limit += 1
-
-    def glLoadModel(self, filename, translate = (0,0,0), rotate = (0,0,0), scale = (1,1,1)):
-        self.objects.append(Model(filename,translate,rotate,scale))
-
-    def glRender(self):
-        transformedVerts = []
-
-        for model in self.objects:
-            mMat = self.glModelMatrix(model.translate, model.scale)
-
-            for face in model.faces:
-                vertCount = len(face)
-
-                v0 = model.vertices[face[0][0]-1]
-                v1 = model.vertices[face[1][0]-1]
-                v2 = model.vertices[face[2][0]-1]
-
-                if vertCount == 4:
-                    v3 = model.vertices[face[3][0]-1]
-                
-                if self.vertexShader:
-                    v0 = self.vertexShader(v0, modelMatrix = mMat)
-                    v1 = self.vertexShader(v1, modelMatrix = mMat)
-                    v2 = self.vertexShader(v2, modelMatrix = mMat)
-                    if vertCount == 4:
-                        v3 = self.vertexShader(v3, modelMatrix = mMat)
-                
-                transformedVerts.append(v0)
-                transformedVerts.append(v1)
-                transformedVerts.append(v2)
-                if vertCount == 4:
-                    transformedVerts.append(v0)
-                    transformedVerts.append(v2)
-                    transformedVerts.append(v3)
-
-        # for vert in self.vertexBuffer:
-        #     if self.vertexShader:
-        #         transformedVerts.append(self.vertexShader(vert, modelMatrix = self.modelMatrix))
-        #     else:
-        #         transformedVerts.append(vert)
-        
-        primitives = self.glPrimitiveAssembly(transformedVerts)
-
-        primColor = None
-        if self.fragmentShader:
-            primColor = self.fragmentShader()
-            primColor = color(primColor[0],
-                              primColor[1],
-                              primColor[2])
-        else:
-            primColor = self.currColor
-
-        for prim in primitives:
-            if self.primitiveType == TRIANGLES:
-                self.glTriangle(prim[0],prim[1],prim[2], primColor)
-
-
 
     def glFinish(self, filename):
         with open(filename,"wb") as file:
